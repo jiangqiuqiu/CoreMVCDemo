f1c06829f444455462a8d4150ab665d5c1471cae
jiangqiuqiu <jiangqiuqiu1984@163.com>
2019-09-21 21:56:27 +0800

    新建立AppDbContext

---
+++ DataAccess/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CoreMVCDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVCDemo.DataAccess
+{
+    public class AppDbContext:DbContext
+    {
+        //DbContextOptions 实例承载配置信息，如连接字符串，数据库提供商使用等
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
