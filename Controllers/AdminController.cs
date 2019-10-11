a9875dfea9b42c73f0f14b5c64f0528148decbff
jiangqiuqiu <jiangqiuqiu1984@163.com>
2019-10-11 08:18:25 +0800

    在 ASP.Net Core 中添加或删除角色中的用户

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -121,5 +121,97 @@ namespace CoreMVCDemo.Controllers
 
             return View(model);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUsersInRole(string roleId)
+        {
+            ViewBag.roleId = roleId;
+
+            var role =await roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"角色id为{roleId}的信息不存在，请重试！";
+                return View("NotFound");
+            }
+
+            var model = new List<UserRoleViewModel>();
+            foreach (var user in userManager.Users)
+            {
+                var userRoleViewModel = new UserRoleViewModel
+                {
+                    UserId=user.Id,
+                    UserName=user.UserName
+                };
+
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    userRoleViewModel.IsSelected = true;
+                }
+                else
+                {
+                    userRoleViewModel.IsSelected = false;
+                }
+
+                model.Add(userRoleViewModel);
+            }
+
+            
+
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditUsersInRole(List<UserRoleViewModel> model,string roleId)
+        {
+            var role = await roleManager.FindByIdAsync(roleId);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"角色id为{roleId}的信息不存在，请重试！";
+                return View("NotFound");
+            }
+
+            for (int i = 0; i < model.Count; i++)
+            {
+                var user = await userManager.FindByIdAsync(model[i].UserId);
+
+                //判断当前用户是否已经属于该角色且已经被选中
+                //不属于的话添加到角色中
+                //没有选中要移除出来
+                var isInRole = await userManager.IsInRoleAsync(user,role.Name);
+                IdentityResult result = null;
+
+                //被选中，不属于该角色，这个时候，添加到角色中
+                if (model[i].IsSelected && !isInRole)
+                {
+                    result =await userManager.AddToRoleAsync(user,role.Name);
+                }
+                else if(!model[i].IsSelected && isInRole)
+                { //没有被选中，但是用户已经在角色中，移除出来
+                    result = await userManager.RemoveFromRoleAsync(user,role.Name);
+                }
+                else//被选中，且已经存在角色中，不发生任何改变的数据
+                {
+                    continue;//跳出当次循环
+                }
+
+                if (result.Succeeded)
+                {
+                    //7个总用户数，0开始进行索引。
+                    if (i < (model.Count - 1))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return RedirectToAction("EditRole", new { id = roleId });
+
+                    }
+
+                }
+            }
+
+
+            return RedirectToAction("EditRole", new { id = roleId });
+        }
     }
 }
